df4e1e92ccc720ee3765402d25f6bda1d1acdae0
David F <davidinpy@gmail.com>
2020-09-14 12:05:37 -0400

    Petties Main.js

--- app/screens/Main.js
+++ app/screens/Main.js
@@ -1,17 +1,14 @@
 import React from "react";
-import {Text} from "react"
-import { StatusBar } from 'expo-status-bar';
+import { Text } from "react";
+import { StatusBar } from "expo-status-bar";
 
-import Box from '../componets/Box';
+import Box from "../componets/Box";
 
-
-export default (props) => {
-    return (
-        <Box color = '#f00'>
-            <Text>
-                Open App.js to start woking on your app!
-            </Text>
-            <StatusBar style="auto" />
-        </Box>
-      );
-    }
+export default () => {
+  return (
+    <Box color="#f00">
+      <Text>Open App.js to start woking on your app!</Text>
+      <StatusBar style="auto" />
+    </Box>
+  );
+};
