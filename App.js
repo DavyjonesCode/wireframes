74f9805e1f4d85277e4963a0044d4f2902ec3149
David F <davidinpy@gmail.com>
2020-09-14 12:02:28 -0400

    Creates Login.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import React from "react"
-import Main from "./app/screens/Main"
-
-const App = () => <Main />;
+import React from "react";
+import Main from "./app/screens/Main";
+import Login from "./app/screens/Login";
+const App = () => <Login />;
 
 export default App;
---
+++ app/screens/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Image, Text, Button, View, StyleSheet, TextInput } from "react-native";
+
+const Login = () => {
+  return (
+    <veiw>
+      <Text>Hello world</Text>
+    </veiw>
+  );
+};
+
+export default Login;
